4cbf75975bd3cc1af2b19962e0c30457edc55c5f
harrival56 <harrival56@gmail.com>
2022-04-28 22:34:10 -0500

    completed this exceercise

--- models/customer.js
+++ models/customer.js
@@ -29,6 +29,10 @@ class Customer {
     return results.rows.map(c => new Customer(c));
   }
 
+  get fullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+
   /** get a customer by ID. */
 
   static async get(id) {
